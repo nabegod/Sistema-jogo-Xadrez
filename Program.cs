a3bd0c5f4623375bdd913dff0e02e0c986cb7114
Nabegod <anabeatrizsouzaalves94@gmail.com>
2025-09-11 13:28:38 -0300

    Placing pieces on the board

--- Program.cs
+++ Program.cs
@@ -1,15 +1,22 @@
 ﻿using System;
-using System.Threading.Tasks;
 using tabuleiro;
+using xadrez;
 
 namespace xadrez_console
 {
     class Program
     {
         static void Main(string[] args)
-        {
+        {   
             Tabuleiro tab = new Tabuleiro(8, 8);
+
+        tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
+        tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 3));
+        tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(2, 4));
+
+
             Tela.imprimirTabuleiro(tab);
+
             Console.ReadLine();
             
         }
