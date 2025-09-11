600ed2649603bddf9bf202aab39edbb0f32b0382
Nabegod <anabeatrizsouzaalves94@gmail.com>
2025-09-11 12:27:44 -0300

    starting to implement the method that prints the board

--- Tabuleiro/Peca.cs
+++ Tabuleiro/Peca.cs
@@ -7,12 +7,14 @@ namespace tabuleiro
         public int qteMovimentos { get; protected set; }
         public Tabuleiro tab { get; protected set; }
 
-        public Peca( Posicao posicao, Tabuleiro tab, Cor cor)
+        public Peca(Posicao posicao, Tabuleiro tab, Cor cor)
         {
-            this.posicao = null;
+            this.posicao = posicao;
             this.tab = tab;
             this.cor = cor;
             this.qteMovimentos = 0;
-        }   
+        }
+
+    
     }
 }
