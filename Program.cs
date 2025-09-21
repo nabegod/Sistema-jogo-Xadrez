bd58d453b322c254a83786d2f98741b206ec7d92
Nabegod <anabeatrizsouzaalves94@gmail.com>
2025-09-21 16:07:04 -0300

    handling errors with custom exception

--- Program.cs
+++ Program.cs
@@ -7,18 +7,29 @@ namespace xadrez_console
     class Program
     {
         static void Main(string[] args)
-        {   
-            Tabuleiro tab = new Tabuleiro(8, 8);
+        {
+            try
+            {
+                Tabuleiro tab = new Tabuleiro(8, 8);
 
-        tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
-        tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 3));
-        tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(2, 4));
+                tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
+                tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
+                tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(2, 4));
 
 
-            Tela.imprimirTabuleiro(tab);
+                Tela.imprimirTabuleiro(tab);
 
-            Console.ReadLine();
-            
+
+            }
+            catch (TabuleiroException e)
+            {
+                 Console.WriteLine(e.Message);
+
+            }
+
+                Console.ReadLine();
+               
         }
+        
     }
 }
