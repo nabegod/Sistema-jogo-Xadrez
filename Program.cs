0cae621a8ce69fedaf5b94e83cfcd8bb50a09519
Nabegod <anabeatrizsouzaalves94@gmail.com>
2025-09-22 00:18:17 -0300

    Created Class PosicaoXadrez

--- Program.cs
+++ Program.cs
@@ -8,28 +8,20 @@ namespace xadrez_console
     {
         static void Main(string[] args)
         {
-            try
-            {
-                Tabuleiro tab = new Tabuleiro(8, 8);
+            PosicaoXadrez pos = new PosicaoXadrez('c', 7);
 
-                tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
-                tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
-                tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(2, 4));
+            Console.WriteLine(pos);
+            Console.WriteLine(pos.toPosicao());
 
+            Console.ReadLine();
 
-                Tela.imprimirTabuleiro(tab);
-
-
-            }
-            catch (TabuleiroException e)
-            {
-                 Console.WriteLine(e.Message);
-
-            }
-
-                Console.ReadLine();
-               
+            
+              
         }
+
+             
+               
+    
         
     }
 }
